e5765dbc032f5647ac24576c06cdba48cce0e7aa
juandsierra82 <juandsierra82@gmail.com>
2015-11-24 08:38:30 -0600

    starting on signup functionality

--- client/www/js/app.js
+++ client/www/js/app.js
@@ -6,11 +6,23 @@
 		$urlRouterProvider.otherwise('/');
 
 		$stateProvider
-		.state('login', {
-      templateUrl: 'views/login.html',
-      url: '/login',
-      controller: 'AuthController'
-    })
+		  .state('signup', {
+        templateUrl: 'views/signup.html',
+        url: '/signup',
+        controller: 'AuthController'
+      })
+
+			.state('login', {
+      	templateUrl: 'views/login.html',
+      	url: '/login',
+      	controller: 'AuthController'
+    	})
+
+    	.state('create', {
+    		templateUrl: 'views/create.html',
+    		url:'/create',
+    		controller: 'CreateController'
+    	})
 
 	});
 })()
--- client/www/js/controllers.js
+++ client/www/js/controllers.js
@@ -1,6 +1,32 @@
 angular.module('gloria.controllers', [])
 
-.controller('AuthController', function ($scope, $route, $location) {
+.controller('AuthController', function ($scope, $route, $location, $Auth) {
+//user object	
+	$scope.user = {};
+	$scope.user.err = '';
+	console.log('in AuthController')
+
+//login functionality expects user data from the db
+	$scope.login = function (){
+		Auth.login($scope.user)
+			.then(function (data){
+				$location.path('/dash')
+			})
+	}
+
+//signup functionality expects auth token from the db
+
+	$scope.signup = function (){
+		Auth.signup($scope.user)
+			.then(function (token){
+				$location.path('/create')
+			})
+	}
+})
+
+.controller('CreateController', function ($scope){
+	console.log('in Create Controller')
+
+})
 
 
-});

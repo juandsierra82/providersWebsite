88d9a39a1adc63cabe5b4c856bf3c6e1a77340c3
juandsierra82 <juandsierra82@gmail.com>
2015-11-26 11:51:33 -0500

    userRoutes signin routes added

--- server/db/userModel/userRoutes.js
+++ server/db/userModel/userRoutes.js
@@ -3,6 +3,7 @@ var userController = require('./userController.js');
 module.exports = function (app){
 
 	app.post('/signup', userController.signup);
-	app.post('/signin', userController.login);
+	app.post('/login', userController.login);
+	app.get('/logout', userController.checkAuth, userController.logout);
 
 }
